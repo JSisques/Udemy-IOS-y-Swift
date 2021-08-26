2fa18200bdef113942a7e7d8210cbeab518713df
Javi Plaza <javierplazasisques@gmail.com>
2021-08-26 12:11:27 +0200

    Seccion 4: Tipo de dato String

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 4/TipoDeDatoString.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//String en una linea
+let myString = "Bienvenidos al curso de Swift"
+
+//String en varias lineas
+let myMultipleString = """
+    Bienvenidos al curso de Swift
+    
+    Esto es un String multilinea
+    """
+
+//String en varias lineas unicamente en codigo
+let myFalseMultipleString = """
+    Bienvenidos al curso de Swift\
+    \
+    Esto es un String multilinea
+    """
+
+//String vacio
+let myEmptyString = ""
+let myEmptyString2 = String()
