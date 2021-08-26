0d1678f4869a303b8f6f59c67adc0ef468c52d28
Javi Plaza <javierplazasisques@gmail.com>
2021-08-26 12:18:56 +0200

    Seccion 4: Tipo de dato Character, Unicode y Emoji

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 4/CaracteresUnicodeEmojis.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation //Framework de los Strings
+
+//Caracter
+let myCaracter: Character = "\u{E9}"
+let myCaracter2 = "\u{24}"
+
+//Caracteres especiales (\n, \r, \t, \0, \')
+let myString = "Bienvenidos al\n curso"
+
+//Emojis
+let myEmoji = "\u{1F496}"
+let myEmoji2 = "😱"
+
