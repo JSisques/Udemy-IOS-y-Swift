aa518d607f65bad74d4e2dfe6dc7a79cd53fc862
Javi Plaza <javierplazasisques@gmail.com>
2021-08-26 11:32:36 +0200

    Seccion 3: Enteros, flotantes y operadores aritmeticos

---
+++ CursoSwift/CursoSwift/Playgrounds/DataTypes.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//Podemos usar la tecla option para saltar a la documentacion
+
+//Enteros
+
+var x = 18
+
+//Flotantes
+
+var y = 18.8
+
+//Operadores aritmeticos
+
+var result1 = x + 2
+var result2 = y + 2.2
+var result3 = Double(x) + y
+var result4 = Double(x) / 5
