732651303d2682266463e234042cb499ee240e0d
Javi Plaza <javierplazasisques@gmail.com>
2021-08-26 12:27:18 +0200

    Seccion 4: Concatenacion e interpolacion de Strings

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 4/ConcatenacionInterpolacion.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Concatenacion
+var myString = "Bienvenidos al" + " " + "curso de Swift"
+myString += "\nholiwis"
+
+let name = "Javi"
+let country = "España"
+let myPersonalInfo = "Mi nombre es " + name + " y vivo en " + country
+
+//Interpolacion
+let myPersonalInfoWithInterpolation = "Mi nombre es \(name) y vivo en \(country)"
