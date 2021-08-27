a3fb1ddf82460368e5b822853ae920bce33dac14
Javi Plaza <javierplazasisques@gmail.com>
2021-08-27 13:47:23 +0200

    Seccion 5: Condiciones anidadas

--- CursoSwift/CursoSwift/Playgrounds/Seccion 5/IfElse.playground/Contents.swift
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 5/IfElse.playground/Contents.swift
@@ -1,11 +1,14 @@
 import UIKit
 
 var edad = 17
-var dinero = 1000
+var dinero = 100
 var sexoFemenino = false
+var ganas = 10
 
 if (edad >= 18 || dinero > 250) && sexoFemenino{
     print("Puedes entrar al bar")
+}else if ganas >= 5{
+    print("Puedes pasaaaar")
 }else{
     print("No puedes entrar al bar")
 }
