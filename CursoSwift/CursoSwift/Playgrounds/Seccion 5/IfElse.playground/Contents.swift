57fa06871a82b0d55f4dad800e7ae1458ad2ce38
Javi Plaza <javierplazasisques@gmail.com>
2021-08-27 13:45:10 +0200

    Seccion 5: If-Else

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 5/IfElse.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var edad = 17
+var dinero = 1000
+var sexoFemenino = false
+
+if (edad >= 18 || dinero > 250) && sexoFemenino{
+    print("Puedes entrar al bar")
+}else{
+    print("No puedes entrar al bar")
+}
