2d8a1e09204403fa0808519047aa27a841e04c9f
Javi Plaza <javierplazasisques@gmail.com>
2021-08-27 12:31:57 +0200

    Seccion 5: Booleanos y valores lógicos

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 5/Booleanos.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let estudiosJavi = "Grado Superior"
+
+let cedulaProfesional = true
+let cedulaMaestria = false
+
+var salario = 0
+
+if cedulaProfesional{
+    salario = 9000
+}else{
+    salario = 8000
+}
+
+print(salario)
