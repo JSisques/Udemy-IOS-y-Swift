83ba76a7af197a35ad3319136c734006d046d298
Javi Plaza <javierplazasisques@gmail.com>
2021-08-27 19:49:06 +0200

    Seccion 5: Bucle while

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 5/Bucle While.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var age = 0
+
+while age < 18{
+    print("Eres menor de edad, te faltan \(18 - age) para ser mayor de edad")
+    age += 1
+}
+
+print("Ahora tienes \(age) y por lo tanto eres mayor de edad")
