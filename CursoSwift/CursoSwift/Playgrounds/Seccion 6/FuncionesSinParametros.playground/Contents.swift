ca8ee594b166e6ece4b8b3b376bde4bc59b83891
Javi Plaza <javierplazasisques@gmail.com>
2021-08-29 10:31:36 +0200

    Seccion 6: Funciones sin parámetro

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 6/FuncionesSinParametros.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//var numeroAleatorio: Int = Int(arc4random_uniform(100))
+
+func holaMundo(){
+    print("Hola mundo!")
+}
+
+holaMundo()
+
+func sumarDosNumerosCualquiera(){
+    
+    var num = 2+2
+    print(num)
+}
+
+sumarDosNumerosCualquiera()
