d1c01bfd572e680af1a87fd2d24e867072b53a92
Javi Plaza <javierplazasisques@gmail.com>
2021-08-30 17:48:07 +0200

    Seccion 8: Protocolo Delegado

--- CursoSwift/CursoSwift/Playgrounds/Seccion 8/Protocolos.playground/Contents.swift
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 8/Protocolos.playground/Contents.swift
@@ -39,3 +39,34 @@ let myTeacher = Teacher(name: "Pepe", age:  40, subject: "Matemáticas")
 
 myProgrammer.fullName()
 myTeacher.fullName()
+
+
+class FirstClass: SecondClassProtocol{
+    func call() {
+        print("Estoy de vuelta")
+    }
+    
+    func callSecond(){
+        let secondClass = SecondClass()
+        secondClass.delegate = self
+        secondClass.callFirst()
+    }
+}
+
+protocol SecondClassProtocol{
+    func call()
+}
+
+class SecondClass{
+    
+    var delegate: SecondClassProtocol?
+    
+    func callFirst(){
+        sleep(5)
+        delegate?.call()
+    }
+    
+}
+
+let firstClass = FirstClass()
+firstClass.callSecond()
