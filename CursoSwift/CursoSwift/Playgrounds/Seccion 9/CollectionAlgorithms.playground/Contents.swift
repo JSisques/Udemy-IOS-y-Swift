0bed35fce934da47a146b7ee2190beaf6fd32260
Javi Plaza <javierplazasisques@gmail.com>
2021-09-01 17:07:24 +0200

    Seccion 9: Algoritmos de colecciones, ordenar datos sorted

--- CursoSwift/CursoSwift/Playgrounds/Seccion 9/CollectionAlgorithms.playground/Contents.swift
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 9/CollectionAlgorithms.playground/Contents.swift
@@ -4,6 +4,7 @@ var myArray = [5, 8, 1, 0, 3, 9, 7, 2, 4, 6]
 var myDictionary = [5: "Cinco", 8: "Ocho", 1: "Uno", 0: "Cero", 3: "Tres", 9: "Nueve", 7: "Siete", 2: "Dos", 4: "Cuatro", 6: "Seis"]
 var mySet: Set = [5, 8, 1, 0, 3, 9, 7, 2, 4, 6]
 
+/*
 //Sort
 print(myArray)
 myArray.sort()
@@ -15,3 +16,27 @@ myArray.sort{ (intA, intB) -> Bool in
 
 //myDictionary.sort()
 //mySet.sort
+*/
+
+//Sorted
+print(myArray)
+var myArraySorted = myArray.sorted() //Ordena y retorna una nueva sin modificar la original
+print(myArray)
+print(myArraySorted)
+
+myArraySorted = myArray.sorted{ (intA, intB) -> Bool in
+    return intA > intB
+}
+
+let mySortedDictionary = myDictionary.sorted{ (elementA, elementB) -> Bool in
+    return elementA > elementB
+}
+print(mySortedDictionary)
+
+var mySortedSet = mySet.sorted()
+print(mySortedSet)
+
+mySortedSet = mySet.sorted{ (intA, intB) -> Bool in
+    return intA > intB
+}
+print(mySortedSet)
