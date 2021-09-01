5209d3c93a7823790454a8f766edaa5a0681dd01
Javi Plaza <javierplazasisques@gmail.com>
2021-09-01 16:08:10 +0200

    Seccion 8: Asociatividad y operadores avanzados

---
+++ CursoSwift/CursoSwift/Playgrounds/Seccion 8/AdvancedOperators.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//Precedencia y asociatividad
+let myInt: Int = 2 + 3 % 4 * 5
+let myInt2: Int = ((2 + 3) / 4) * 5
+
+//Más operadores avanzados
+// - Operadores bit a bit: Realizar operaciones con bits de tipo NOT, AND, OR, XOR
+// - Operadores de desplazamiento: Para desplazar bits
+// - Operadores de desbordamiento: Para manejar errores en valores por encima de su limite
+// - Operadores custom: Para definir nosotros mismos los operadores 
