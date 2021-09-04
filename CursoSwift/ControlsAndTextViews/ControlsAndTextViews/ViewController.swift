8449a18bff3a25c030deadbb26505106fd6e9cdd
Javi Plaza <javierplazasisques@gmail.com>
2021-09-04 15:15:25 +0200

    Seccion 12: Pickers

--- CursoSwift/ControlsAndTextViews/ControlsAndTextViews/ViewController.swift
+++ CursoSwift/ControlsAndTextViews/ControlsAndTextViews/ViewController.swift
@@ -11,13 +11,23 @@ class ViewController: UIViewController {
     
     //Outlets
     @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    
+    //Variables
+    private let myPickerViewValues = ["Uno", "Dos", "Tres", "Cuatro", "Cinco"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Buttons
         myButton.setTitle("Mi botón", for: .normal)
         myButton.backgroundColor = .blue
         myButton.setTitleColor(.white, for: .normal)
+        
+        //Pickers
+        myPickerView.backgroundColor = .lightGray
+        myPickerView.dataSource = self
+        myPickerView.delegate = self
         
     }
     
@@ -32,3 +42,31 @@ class ViewController: UIViewController {
     
 }
 
+//El protocolo DataSource sirve para cargar los datos y el Delegate para interactuar con nuestro picker
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    //Función para devolver el numero de componentes (columnas)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        //Queremos que solo tenga 1 columna
+        return 1
+    }
+    
+    //Numero de filas del Picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myPickerViewValues.count
+    }
+    
+    //Representacion visual de los elementos
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myPickerViewValues[row]
+    }
+    
+    //Esta operacion se invoca cada vez que tengamos una fila seleccionada
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let myString = myPickerViewValues[row]
+        myButton.setTitle(myString, for: .normal)
+    }
+    
+    
+}
