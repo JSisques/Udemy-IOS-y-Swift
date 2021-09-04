edfa06105f9d6ae917eabcb08abda38c4498849d
Javi Plaza <javierplazasisques@gmail.com>
2021-09-04 14:34:06 +0200

    Seccion 12: Buttons

--- CursoSwift/ControlsAndTextViews/ControlsAndTextViews/ViewController.swift
+++ CursoSwift/ControlsAndTextViews/ControlsAndTextViews/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myButton.setTitle("Mi botón", for: .normal)
+        myButton.backgroundColor = .blue
+        myButton.setTitleColor(.white, for: .normal)
+        
     }
-
-
+    
+    //Actions
+    @IBAction func myButtonAction(_ sender: Any) {
+        if myButton.backgroundColor == .blue{
+            myButton.backgroundColor = .green
+        } else{
+            myButton.backgroundColor = .blue
+        }
+    }
+    
 }
 
