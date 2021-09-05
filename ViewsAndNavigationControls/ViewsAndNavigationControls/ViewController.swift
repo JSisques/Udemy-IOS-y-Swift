1cedec6afdadd2ec89bdc0c7999058aa48aea602
Javi Plaza <javierplazasisques@gmail.com>
2021-09-05 10:43:30 +0200

    Seccion 14: Views

---
+++ ViewsAndNavigationControls/ViewsAndNavigationControls/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ViewsAndNavigationControls
+//
+//  Created by Javier Plaza Sisqués on 5/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
