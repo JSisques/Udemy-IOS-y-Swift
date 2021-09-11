c7284ae8fdd6257709f82cc931b01b5a0aa73bbe
Javi Plaza <javierplazasisques@gmail.com>
2021-09-11 13:29:31 +0200

    Seccion 14: Map Views

--- ViewsAndNavigationControls/ViewsAndNavigationControls/ViewController.swift
+++ ViewsAndNavigationControls/ViewsAndNavigationControls/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import WebKit
+import MapKit
 
 class ViewController: UIViewController {
     
@@ -14,10 +15,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var myFakeView: UIView!
     @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var myMapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         myWebView.load(URLRequest(url: URL(string: "https://www.google.es")!))
+        
+        myMapView.centerCoordinate = CLLocationCoordinate2D(latitude: 42.60003, longitude: -5.57032)
     }
     
     @IBAction func myButtonAction(_ sender: Any) {
