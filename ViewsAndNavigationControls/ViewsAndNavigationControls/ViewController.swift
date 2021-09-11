369a6f15693ec5de943f2bbeef1e32ede0a7570c
Javi Plaza <javierplazasisques@gmail.com>
2021-09-11 12:18:52 +0200

    Seccion 14: Stack Views

--- ViewsAndNavigationControls/ViewsAndNavigationControls/ViewController.swift
+++ ViewsAndNavigationControls/ViewsAndNavigationControls/ViewController.swift
@@ -8,15 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myView: UIView!
-    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func myButtonAction(_ sender: Any) {
+        myView.isHidden = true
+    }
 }
 
