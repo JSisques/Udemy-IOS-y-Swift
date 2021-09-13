7799adda3ed35018389795116979f639a0751c85
Javi Plaza <javierplazasisques@gmail.com>
2021-09-13 20:07:51 +0200

    Seccion 15: Introduccion a la navegacion de pantallas en IOS

---
+++ Navegacion/Navegacion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navegacion
+//
+//  Created by Javier Plaza Sisqués on 13/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
