4bb9250fc3a0b2c39b2d4680e04b016f9c3dcc7c
Javi Plaza <javierplazasisques@gmail.com>
2021-09-14 10:00:09 +0200

    Seccion 16: UITableView

--- TableViews/TableViews/ViewController.swift
+++ TableViews/TableViews/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
