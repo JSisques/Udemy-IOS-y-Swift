a16d39e48e04b209200ddb29403d35bcacfb4d72
Javi Plaza <javierplazasisques@gmail.com>
2021-09-15 12:36:25 +0200

    Seccion 18: #Function, Override y Super

---
+++ LifeCicle/LifeCicle/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LifeCicle
+//
+//  Created by Javier Plaza Sisqués on 15/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //El #function simplemente indica en que función estas
+        print("Estoy en la VC1 y se activo --> \(#function)")
+    }
+
+
+}
+
