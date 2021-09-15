89969a5b2634fbbbe625aad28bbaf7abba12bde9
Javi Plaza <javierplazasisques@gmail.com>
2021-09-15 18:55:33 +0200

    Seccion 19

---
+++ AdvancedAutoLayout/AdvancedAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvancedAutoLayout
+//
+//  Created by Javier Plaza Sisqués on 15/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
