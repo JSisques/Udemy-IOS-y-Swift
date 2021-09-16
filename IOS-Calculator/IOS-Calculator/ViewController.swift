44a191a2d4292067d7522c6a97d8ac170fdbde92
Javi Plaza <javierplazasisques@gmail.com>
2021-09-16 13:04:56 +0200

    Seccion 20: Creación del proyecto

---
+++ IOS-Calculator/IOS-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-Calculator
+//
+//  Created by Javier Plaza Sisqués on 16/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
