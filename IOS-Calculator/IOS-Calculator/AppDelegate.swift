66dbfd87a6e9bac0d8fc3952b7f51de509aef776
Javi Plaza <javierplazasisques@gmail.com>
2021-09-19 12:14:43 +0200

    Seccion 22

--- IOS-Calculator/IOS-Calculator/AppDelegate.swift
+++ IOS-Calculator/IOS-Calculator/AppDelegate.swift
@@ -10,11 +10,25 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //Setup
+        setupView()
+        
         return true
+    }
+    
+    //MARK: -Private methods
+    
+    private func setupView(){
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
     }
 
 }
---
+++ IOS-Calculator/IOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  IOS-Calculator
+//
+//  Created by Javier Plaza Sisqués on 16/9/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    //MARK: - Initialitation
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
