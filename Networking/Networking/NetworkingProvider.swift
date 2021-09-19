f76d7c926b90a92dc24762ce16bcab3bff41d5a4
Javi Plaza <javierplazasisques@gmail.com>
2021-09-19 12:18:02 +0200

    Seccion 22: Uso de Swift package manager e integración con la libreria Alamofire

---
+++ Networking/Networking/NetworkingProvider.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkingProvider.swift
+//  Networking
+//
+//  Created by Javier Plaza Sisqués on 19/9/21.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkingProvider{
+    
+    //Para crear un objeto singleton
+    static let shared = NetworkingProvider()
+}
